c85bb75dbe1b9e0e9374c91923bb4cabb5594c58
hubwriter <hubwriter@github.com>
2024-08-15 16:47:14 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 code --install-extension yoshinorin.current-file-path
 code --install-extension sleistner.vscode-fileutils
 code --install-extension peterbe.ghdocs-goer
-code --install-extension esbenp.prettier-vscode
